b3c4a9b5f358d3275d227c1d9542dee4172517dc
Viniciusm15 <viniciushiagomartins00@gmail.com>
2025-04-07 11:54:09 -0300

    feat: estrutura inicial do projeto co Spring boot.

---
+++ src/main/java/com/vinicius/serversapi/ServersApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.serversapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServersApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinicius/serversapi/ServersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.serversapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
